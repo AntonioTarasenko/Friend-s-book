31b9cff1a220464375887b8355b9303e67e2f7d0
AntonioTarasenko <toxata1@gmail.com>
2025-01-08 15:17:07 +0200

    experiment whith HATE

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import Notification from '../models/notification.model.js';
 import User from '../models/user.model.js';
 import Post from '../models/post.model.js';
 import { v2 as cloudinary } from 'cloudinary';
@@ -64,29 +65,103 @@ export const deletePost = async (req, res) => {
 };
 
 export const commentOnPost = async (req, res) => {
-    try {
-        const { text } = req.body;
-        const postId = req.params.id;
-        const userId = req.user._id;
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
 
-        if(!text) {
-            return res.status(400).json({ error: 'Text field is required' });
-        }
-        const post = await Post.findById(postId)
-
-        if(!post) {
-            return res.status(404).json({ error: 'Post not found' });
-        }
-
-        const comment = {user: userId, text};
-
-        post.comments.push(comment);
-        await post.save();
-
-        res.status(200).json(post);
-    } catch (error) {
-        console.log('Error in commentOnPost controller: ', error);
-        res.status(500).json({ error: 'Internal server error' });
-        
+    if (!text) {
+      return res.status(400).json({ error: 'Text field is required' });
     }
-}
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+
+    const comment = { user: userId, text };
+
+    post.comments.push(comment);
+    await post.save();
+
+    res.status(200).json(post);
+  } catch (error) {
+    console.log('Error in commentOnPost controller: ', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const likeUnlikePost = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { id: postId } = req.params;
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+
+    const userLikedPost = post.likes.includes(userId);
+
+    if (userLikedPost) {
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      res.status(200).json({ message: 'Post unliked successfully' });
+    } else {
+      post.likes.push(userId);
+      await post.save();
+
+      const notification = new Notification({
+        from: userId,
+        to: post.user,
+        type: 'like',
+      });
+      await notification.save();
+
+      res.status(200).json({ message: 'Post liked successfully' });
+    }
+  } catch (error) {
+    console.log('Error in likeUnlikePost controller: ', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+
+
+
+
+export const hateUnhatePost = async (req, res) => {
+    try {
+      const userId = req.user._id;
+      const { id: postId } = req.params;
+  
+      const post = await Post.findById(postId);
+  
+      if (!post) {
+        return res.status(404).json({ error: 'Post not found' });
+      }
+  
+      const userHatedPost = post.hates.includes(userId);
+  
+      if (userHatedPost) {
+        await Post.updateOne({ _id: postId }, { $pull: { hates: userId } });
+        res.status(200).json({ message: 'Post unhated successfully' });
+      } else {
+        post.hates.push(userId);
+        await post.save();
+  
+        const notification = new Notification({
+          from: userId,
+          to: post.user,
+          type: 'hate',
+        });
+        await notification.save();
+  
+        res.status(200).json({ message: 'Post hated successfully' });
+      }
+    } catch (error) {
+      console.log('Error in hateUnhatePost controller: ', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+  
